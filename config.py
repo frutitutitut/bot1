8ed4da179ddc6f298d684f0fa5af8bf7af3cf9b2
frutitutitut <dymokks.kote@gmail.com>
2020-08-08 10:15:06 +0300

    fix

---
+++ config.py
@@ -0,0 +1,7 @@
+# import pyTelegramBotAPI
+import telebot
+
+# Token, chat_id, user_id (@netslovodniemoji)
+bot = telebot.TeleBot('1131596371:AAHvwR0Lg89OLyllMgPAdcqeMlWyB247w-s')
+chat_id = "-1001496091669"
+user_id = "655044942"
